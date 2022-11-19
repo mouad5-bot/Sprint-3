a26a5800c75f23ec8d56beec9a8f2332eb8b7430
MOUAD FIFEL <mouad.fifel5@gmail.com>
2022-11-19 02:09:13 +0100

    regler les navigation dans tout les pages

--- Système de gestion des produits de gaming/includes/navbar.php
+++ Système de gestion des produits de gaming/includes/navbar.php
@@ -1,13 +1,23 @@
 
 	<!-- BEGIN #navbar -->
-		<div class="navbar navbar-dark bg-dark justify-content-between ">
-			<div class="logo"> 
-				<a href="../index.php"><span class="ms-4"> <img src="../Assets/image/logo.png" alt="logo"> </span></a>
-			</div>
-			<div class="d-flex align-items-center h4">
-				<span class="text-white "> <i class="bi bi-person-circle "></i> Admin</span>
-			</div>	
-			<div class="d-flex align-items-center h5 me-5">
-				<a href="../pages/login.php"> <i class="bi bi-box-arrow-right">logout</i> </a>
-			</div>	
+	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
+		<div class="logo"> 
+			<a href="../index.php"><span class="ms-4"> <img src="../Assets/image/logo.png" alt="logo"> </span></a>
 		</div>
+		<div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01" >
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item active h4">
+					<a class="nav-link text-white" href="#">Home </a>
+				</li>
+				<li class="nav-item h4">
+					<a class="nav-link text-white" href="#">Profil</a>
+				</li>
+				<li class="nav-item h4">
+					<a class="nav-link text-white" href="#">Products</a>
+				</li>
+			</ul>
+		</div>
+		<div class="d-flex align-items-center h5 me-5 ">
+			<a href="../config/scripts.php?logout=1" name="logout">  <i class="bi bi-box-arrow-right logout">logout</i> </a>
+		</div>
+	</nav>
--- Système de gestion des produits de gaming/index.php
+++ Système de gestion des produits de gaming/index.php
@@ -21,17 +21,32 @@
 	</head>
 <body>
 	<!-- BEGIN #navbar -->
-	<div class="navbar navbar-dark bg-dark justify-content-between ">
+	<nav class="navbar navbar-expand-lg navbar-light bg-dark">
 		<div class="logo"> 
 			<a href="index.php"><span class="ms-4"> <img src="./Assets/image/logo.png" alt="logo"> </span></a>
 		</div>
-		<div class=" d-flex align-items-center h4">
-		</div>	
-		<div class="d-flex align-items-center h5 me-5">
-			<!-- <span class="text-white "> <i class="bi bi-box-arrow-right"></i> logout</span> -->
-			<a href="config/scripts.php?logout=1" name="logout">  <i class="bi bi-box-arrow-right ">logout</i> </a>
-		</div>	
-	</div>
+		<div class="collapse navbar-collapse justify-content-center" id="navbarTogglerDemo01" >
+			<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+				<li class="nav-item active h4">
+					<a class="nav-link text-white" href="#">Home </a>
+				</li>
+				<li class="nav-item h4">
+					<a class="nav-link text-white" href="#">Profil</a>
+				</li>
+				<li class="nav-item h4">
+					<a class="nav-link text-white" href="#">Products</a>
+				</li>
+			</ul>
+		</div>
+		<div class="d-flex align-items-center h5 me-5 ">
+			<a href="config/scripts.php?logout=1" name="logout">  <i class="bi bi-box-arrow-right logout">logout</i> </a>
+		</div>
+	</nav>
+
+
+
+
+
 	<!-- FINISHED #navbar -->
 
     <main class="container">
